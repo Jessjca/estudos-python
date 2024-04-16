2276ea0e73ad7f248b24c48b8ce799ec80f65d92
Jessjca <juzyres@gmail.com>
2024-04-16 19:06:54 -0300

    Atualiza resolução do exercício 24
    Co-Authored-By: Caio Paulino Costa <130261742+CaioPaulinoCosta@users.noreply.github.com>

--- exercicios-jessica/ex024.py
+++ exercicios-jessica/ex024.py
@@ -1,6 +1,6 @@
-cidade = input('Digite a cidade: ')
+cidade = input('Digite a cidade: ').lower().strip()
 
-if cidade.find('Santo') in [0]:
+if (cidade.find('santo')) in [0]:
         print('A cidade começa com Santo')
 else:
     print('A cidade não começa com Santo')
