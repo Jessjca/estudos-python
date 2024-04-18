12a30cfa78c9a76a998006d123fdc8c40c818a05
Jessjca <juzyres@gmail.com>
2024-04-18 16:26:16 -0300

    Alterando exercício 32,33,34
    Co-Authored-By: Caio Paulino Costa <130261742+CaioPaulinoCosta@users.noreply.github.com>

--- exercicios-jessica/ex032.py
+++ exercicios-jessica/ex032.py
@@ -1,6 +1,9 @@
-ano = int(input('Digite o ano: '))
-if ano % 4 == 0:
-    if ano % 100 != 0:
-        print('O ano {} é um ano bissexto!'.format(ano))
+from datetime import date
+ano = int(input('Digite o ano (Para o ano atual, digite 0): '))
+if ano == 0:
+    ano = date.today().year
+
+if ano % 4 == 0 and ano % 100 !=0 or ano % 400 == 0:
+    print('O ano {} é um ano bissexto!'.format(ano))
 else:
     print('O ano {} não é um ano bissexto.')
